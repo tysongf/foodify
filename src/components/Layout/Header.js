3fd5e5fe9346d5a0a95eec91388d1ad28df24064
Tyson G Fritz <tysongfritz@gmail.com>
2022-09-12 12:27:04 -0700

    Initial commit with header completed.

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import { Fragment } from "react";
+import mealsImage from "../../assets/meals.jpg";
+import css from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = () => {
+   return (
+      <Fragment>
+         <header className={css.header}>
+            <h1>Foodify</h1>
+            <HeaderCartButton />
+         </header>
+         <div className={css["main-image"]}>
+            <img src={mealsImage} alt="A table full of delicious food." />
+         </div>
+      </Fragment>
+   );
+};
+
+export default Header;
---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,17 @@
+import React from "react";
+import CartIcon from "./CartIcon";
+import css from "./HeaderCartButton.module.css";
+
+const HeaderCartButton = (props) => {
+   return (
+      <button className={css.button}>
+         <span className={css.icon}>
+            <CartIcon />
+         </span>
+         <span>My Cart</span>
+         <span className={css.badge}>3</span>
+      </button>
+   );
+};
+
+export default HeaderCartButton;
